361771443f0fc811a400df063a088cff22c53d45
miloszchojecki <268484@student.pwr.edu.pl>
2023-06-04 13:15:10 +0200

    added WatchdogThread, LoggerThread and handler of SIGTERM and SIGINT, changed CMakeLists.txt

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 #include <pthread.h>
 #include <stdbool.h>
 #include <stdatomic.h>
-
+#include <signal.h>
+#include <assert.h>
 
 #define MAX_LINE_LENGTH 256
 #define MAX_LABEL_LENGTH 8
@@ -33,6 +34,12 @@ typedef struct {
     pthread_mutex_t mutex;
     double *usage;
 } ThreadData;
+
+typedef struct {
+    ThreadData *data;
+    FILE *logFile;
+    pthread_mutex_t mutex;
+} LoggerThreadData;
 
 static void readCpuStats(CpuStats *stats, int core) {
     FILE *file;
@@ -72,7 +79,7 @@ static void *readerThread(void *arg) {
 
     while (isRunning) {
         pthread_mutex_lock(&(data->mutex));
-        memcpy(data->prevStats, data->currStats, sizeof(CpuStats) * (unsigned long)(numOfCores + 1));
+        memcpy(data->prevStats, data->currStats, sizeof(CpuStats) * (unsigned long) (numOfCores + 1));
         for (short i = 0; i < numOfCores; i++) {
             readCpuStats(&currStats[i], i);
         }
@@ -84,8 +91,8 @@ static void *readerThread(void *arg) {
 }
 
 static void *analyzerThread(void *arg) {
+    ThreadData *data = (ThreadData *) arg;
     while (isRunning) {
-        ThreadData *data = (ThreadData *) arg;
         CpuStats *prevStats = data->prevStats;
         CpuStats *currStats = data->currStats;
         long numOfCores = data->numOfCores;
@@ -142,8 +149,8 @@ static void *analyzerThread(void *arg) {
 }
 
 static void *printerThread(void *arg) {
+    ThreadData *data = (ThreadData *) arg;
     while (isRunning) {
-        ThreadData *data = (ThreadData *) arg;
         long numOfCores = data->numOfCores;
         double *usage = data->usage;
         pthread_mutex_lock(&(data->mutex));
@@ -158,6 +165,68 @@ static void *printerThread(void *arg) {
     pthread_exit(NULL);
 }
 
+static void *watchdogThread(void *arg) {
+    ThreadData *data = (ThreadData *) arg;
+    while (isRunning) {
+        sleep(2);
+
+        pthread_mutex_lock(&(data->mutex));
+        CpuStats *prevStats = data->prevStats;
+        CpuStats *currStats = data->currStats;
+        long numOfCores = data->numOfCores;
+        pthread_mutex_unlock(&(data->mutex));
+
+        bool threadsRunning = false;
+        for (short i = 0; i < numOfCores; i++) {
+            if (prevStats[i].user != currStats[i].user ||
+                prevStats[i].nice != currStats[i].nice ||
+                prevStats[i].system != currStats[i].system ||
+                prevStats[i].idle != currStats[i].idle ||
+                prevStats[i].iowait != currStats[i].iowait ||
+                prevStats[i].irq != currStats[i].irq ||
+                prevStats[i].softirq != currStats[i].softirq ||
+                prevStats[i].steal != currStats[i].steal ||
+                prevStats[i].guest != currStats[i].guest ||
+                prevStats[i].guest_nice != currStats[i].guest_nice) {
+                threadsRunning = true;
+                break;
+            }
+        }
+
+        if (!threadsRunning) {
+            isRunning = false;
+            printf("Error: Threads have stopped running. Application terminated.\n");
+        }
+    }
+    pthread_exit(NULL);
+}
+
+static void *loggerThread(void *arg) {
+    LoggerThreadData *loggerData = (LoggerThreadData *) arg;
+    FILE *logFile = loggerData->logFile;
+
+    while (isRunning) {
+        ThreadData *data = loggerData->data;
+        pthread_mutex_lock(&(data->mutex));
+        double *usage = data->usage;
+        long numOfCores = data->numOfCores;
+        pthread_mutex_unlock(&(data->mutex));
+
+        fprintf(logFile, "Overall CPU Usage: %.2f%%\n", usage[0]);
+        for (short i = 0; i < numOfCores; i++) {
+            fprintf(logFile, "Core %d Usage: %.2f%%\n", i, usage[i + 1]);
+        }
+        sleep(1);
+        fflush(logFile);
+    }
+    pthread_exit(NULL);
+}
+
+static void sigHandler(int signum) {
+    printf("\nCaught signal %d\n", signum);
+    isRunning = false;
+}
+
 int main(void) {
     long numOfCores = sysconf(_SC_NPROCESSORS_ONLN);
 
@@ -169,17 +238,37 @@ int main(void) {
 
     pthread_mutex_init(&(data.mutex), NULL);
 
-    pthread_t readerThreadId, analyzerThreadId, printerThreadId;
-
+    pthread_t readerThreadId, analyzerThreadId, printerThreadId, watchdogThreadId, loggerThreadId;
     pthread_create(&readerThreadId, NULL, readerThread, &data);
     pthread_create(&analyzerThreadId, NULL, analyzerThread, &data);
     pthread_create(&printerThreadId, NULL, printerThread, &data);
+    pthread_create(&watchdogThreadId, NULL, watchdogThread, &data);
+
+    FILE *logFile = fopen("log.txt", "w");
+    if (logFile == NULL) {
+        perror("Failed to open log file");
+        exit(1);
+    }
+
+    LoggerThreadData loggerData;
+    loggerData.data = &data;
+    loggerData.logFile = logFile;
+    pthread_create(&loggerThreadId, NULL, loggerThread, &loggerData);
+
+    signal(SIGTERM, sigHandler);
+    signal(SIGINT, sigHandler);
 
     pthread_join(readerThreadId, NULL);
     pthread_join(analyzerThreadId, NULL);
     pthread_join(printerThreadId, NULL);
+    pthread_join(watchdogThreadId, NULL);
+    pthread_join(loggerThreadId, NULL);
 
+    pthread_mutex_destroy(&(data.mutex));
     free(data.prevStats);
     free(data.currStats);
     free(data.usage);
+    fclose(logFile);
+
+    return 0;
 }
